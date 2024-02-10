4f8fe9b33517d44f7bdac34cf6fd1b12be7130b7
GRAYJNR <johngrahamjunior@gmail.com>
2024-02-09 17:48:39 -0800

    wip

--- src/components/AppointmentForm.tsx
+++ src/components/AppointmentForm.tsx
@@ -10,7 +10,6 @@ const countries = [
 const AppointmentForm: React.FC = () => {
   const [name, setName] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
-  const [countryCode, setCountryCode] = useState(countries[0].code);
   const [date, setDate] = useState('');
   const [time, setTime] = useState('');
 
@@ -28,18 +27,22 @@ const AppointmentForm: React.FC = () => {
         value={phoneNumber}
         onChange={(e) => setPhoneNumber(e.target.value)}
       />
-      <select value={countryCode} onChange={(e) => setCountryCode(e.target.value)}>
-        {countries.map((country) => (
-          <option key={country.code} value={country.code}>
-            {country.name}
-          </option>
-        ))}
-      </select>
       <input type="date" placeholder="Date" value={date} onChange={(e) => setDate(e.target.value)} />
       <select value={time} onChange={(e) => setTime(e.target.value)}>
         {/* Populate with your time options */}
+        <option value="07:00">07:00 AM</option>
+        <option value="08:00">08:00 AM</option>
+        <option value="09:00">09:00 AM</option>
         <option value="10:00">10:00 AM</option>
         <option value="11:00">11:00 AM</option>
+        <option value="12:00">12:00 AM</option>
+        <option value="01:00">01:00 PM</option>
+        <option value="02:00">02:00 PM</option>
+        <option value="03:00">03:00 PM</option>
+        <option value="04:00">04:00 PM</option>
+        <option value="05:00">05:00 PM</option>
+        <option value="06:00">06:00 PM</option>
+        <option value="07:00">07:00 PM</option>
         {/* Add more options as needed */}
       </select>
       <button type="submit">Submit</button>
